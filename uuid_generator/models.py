05c12e542602e04deba3fb93873804bf7a60d209
Tumise <oladele.alade@gmail.com>
2021-05-23 09:36:51 +0100

    chore(generator) added random generator model

--- uuid_generator/models.py
+++ uuid_generator/models.py
@@ -1,4 +1,10 @@
 from django.db import models
-
-# Create your models here.
 import uuid
+
+class RandomGenerator(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
