2be8b2fe897ab06b3e2894a0cb7e74031ffb8a89
Tumise <oladele.alade@gmail.com>
2021-05-23 20:29:46 +0100

    ch(stup-test) added test for generate uuid

--- uuid_generator/tests.py
+++ uuid_generator/tests.py
@@ -2,4 +2,13 @@ from django.test import TestCase
 
 import pytest
 
+from uuid_generator.factories import RandomGeneratorFactory
+
 # Create your tests here.
+from uuid_generator.manager import RandomGeneratorDao
+from uuid_generator.models import RandomGenerator
+
+
+def test_generate_uuid(db):
+    RandomGeneratorDao.generate_uuid()
+    assert RandomGenerator.objects.count() == 1
