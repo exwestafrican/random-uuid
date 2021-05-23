88f415ee341ad9619ea65e674e64618cef65a0e9
Tumise <oladele.alade@gmail.com>
2021-05-23 11:06:57 +0100

    rf(generator): made instance datetime string a variable created_at = instance.created_at.strftime(             settings.REST_FRAMEWORK.get("DATETIME_FORMAT")         )

--- uuid_generator/serializers.py
+++ uuid_generator/serializers.py
@@ -5,8 +5,7 @@ from rest_framework import serializers
 
 class RandomGeneratorSerializer(serializers.BaseSerializer):
     def to_representation(self, instance):
-        return {
-            instance.created_at.strftime(
-                settings.REST_FRAMEWORK.get("DATETIME_FORMAT")
-            ): instance.id
-        }
+        created_at = instance.created_at.strftime(
+            settings.REST_FRAMEWORK.get("DATETIME_FORMAT")
+        )
+        return {created_at: instance.id}
