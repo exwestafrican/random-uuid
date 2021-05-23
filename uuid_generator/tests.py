81f980c441f5910a7a6ff58490e3cc7908b4b262
Tumise <oladele.alade@gmail.com>
2021-05-23 20:31:03 +0100

    ch(stup-test) added test for fetch all uuids

--- uuid_generator/tests.py
+++ uuid_generator/tests.py
@@ -12,3 +12,8 @@ from uuid_generator.models import RandomGenerator
 def test_generate_uuid(db):
     RandomGeneratorDao.generate_uuid()
     assert RandomGenerator.objects.count() == 1
+
+
+def test_fetch_all_uuids(db):
+    RandomGeneratorDao.generate_uuid()
+    assert RandomGeneratorDao.fetch_all_uuids().count() == 1
